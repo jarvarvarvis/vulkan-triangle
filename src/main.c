c264909ec282d444a84da46822e1490bc4f7d97e
MisterJaJo <jarvistrigo@gmail.com>
2021-11-30 18:15:39 +0100

    Setup GLFW stuff

--- src/main.c
+++ src/main.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <GLFW/glfw3.h>
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+
 int main() {
-	printf("Hello world!\n");
+	GLFWwindow *window;
+
+	/* Initialize glfw and setup the window */
+	if (!glfwInit())
+		return -1;
+
+	window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT,
+				  "Vulkan Triangle", NULL, NULL);
+
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	/* GLFW main loop */
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	/* Terminate GLFW when the loop was exited */
+	glfwTerminate();
 	return EXIT_SUCCESS;
 }
